5908e82b657571c7fa4ae9ec71f2295dd266b099
nodajida <sniper.nhj@gmail.com>
2024-07-24 22:42:56 +0900

    Level1 - 두 번째 요구사항 완료

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -11,8 +11,17 @@ public class App {
         // Scanner를 사용하여 양의 정수를 입력받고 적합한 타입의 변수에 저장합니다.
         int num2 = sc.nextInt();
 
-        System.out.println("첫 번째 숫자: " + num1);
-        System.out.println("첫 번째 숫자: " + num2);
+        System.out.print("사칙연산 기호를 입력하세요: ");
+        // 사칙연산 기호를 적합한 타입으로 선언한 변수에 저장합니다.
+        // Scanner는 char 타입으로 입력 받을 수 없다
+        //그래서 등장한 것이 바로 charAt()
+        // String으로 저장된 문자열 중에서 한 글자만 선택해서 char 타입으로 변환해주는 녀석
+        char operator = ' ';
+        operator = sc.next().charAt(0);
+        System.out.println("입력받은 기호: " + operator);
+
+
+
 
     }
 }
